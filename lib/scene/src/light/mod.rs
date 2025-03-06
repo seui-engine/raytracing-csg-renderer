98d9c68518561dfb9931739062834735ab62cfc1
unknown <pjs771188@gmail.com>
2025-03-07 03:15:13 +0900

    create directional light and fix lib/sample

--- lib/scene/src/light/mod.rs
+++ lib/scene/src/light/mod.rs
@@ -1,20 +1,24 @@
+use directional::DirectionalLight;
 use point::PointLight;
 use schemars::JsonSchema;
 use serde::Deserialize;
 use seui_engine_raytracing_csg_renderer_core::types::rt::Light;
 
+pub mod directional;
 pub mod point;
 
 #[derive(Clone, Debug, Deserialize, JsonSchema)]
 #[serde(tag = "type", rename_all = "camelCase")]
 pub enum DeserializableLight {
     Point(PointLight),
+    Directional(DirectionalLight),
 }
 
 impl DeserializableLight {
     pub fn into_light(self) -> Box<dyn Light> {
         match self {
             DeserializableLight::Point(c) => Box::new(c),
+            DeserializableLight::Directional(c) => Box::new(c),
         }
     }
 }
